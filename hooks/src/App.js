9439ee5bf1c0260a7d3a46028609d95ef7814e43
aluno <Aluno@PR01-COMP1-09.CSED.LAB>
2023-04-14 21:37:34 -0300

    hooks - useEffect

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './App.css'
 
-import Gallery from './components/useState/Gallery'
+import UseEfect from './components/useEfect/UseEfect'
 
 function App () {
   return (
     <div className='App'>
-      <Gallery />
+      <UseEfect />
     </div>
   )
 }
