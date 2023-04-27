af364ba3e8db524b3a61fc26cc69eddbdda4a92a
Jonathan Henrique Alves de Lima <38362824+JonathanHenriqueCWB@users.noreply.github.com>
2023-04-27 17:51:03 -0300

    filmes - page de cadastro e componente de formulario

--- filmes/src/components/Header/Header.jsx
+++ filmes/src/components/Header/Header.jsx
@@ -1,11 +1,16 @@
 import { Link } from 'react-router-dom'
 import React from "react";
 import './Header.css'
+import { useLocation } from 'react-router-dom';
 
 // Obejto com estilo
 const style ={ "backgroundColor" : "#222", "padding" : "20px" }
 
 const Header = props => {
+
+    const location = useLocation()
+    console.log('voce está em:' + location.pathname)
+
     return (
     <ul className="nav justify-content-center" style={style}>
         <li className="nav-item">
@@ -17,6 +22,11 @@ const Header = props => {
         <li className="nav-item">
             <Link className='nav-link' to='/sobre' style={{color: "#fff"}}>SOBRE</Link>
         </li>
+
+        {
+            location.pathname == '/cadastrar' ? <li></li> : <li className="nav-item"> <Link className='nav-link' to='/cadastrar' style={{color: "#fff"}}>CADASTRAR</Link></li> 
+        }
+        
     </ul>
     )
 }
--- filmes/src/configRoutes.js
+++ filmes/src/configRoutes.js
@@ -4,6 +4,7 @@ import Home from './pages/home'
 import Planos from './pages/planos'
 import Sobre from './pages/sobre'
 import Detalhes from './pages/detalhes'
+import Cadastrar from './pages/cadastrar'
 
 
 export default function ConfigRoutes(){
@@ -12,6 +13,7 @@ export default function ConfigRoutes(){
           <Route path='/' element={<Home></Home>}/> { /* root */ }
           <Route path='/planos' element={<Planos></Planos>}/>
           <Route path='/sobre' element={<Sobre></Sobre>}/>
+          <Route path='/cadastrar' element={<Cadastrar></Cadastrar>}/>
           <Route path='/detalhes/:id' element={<Detalhes></Detalhes>}/>
           <Route path='*' element={<h1>Página Não Encontrada!</h1>} />
         </Routes>
