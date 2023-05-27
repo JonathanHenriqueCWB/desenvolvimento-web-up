79f2a9edc1a9f4443a72c2371cf4186615b52045
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2023-05-27 00:58:32 -0300

    criando os controllers

--- backend/backend-catalogo-express-generator/controllers/movies-controllers.js
+++ backend/backend-catalogo-express-generator/controllers/movies-controllers.js
@@ -1,11 +1,48 @@
 const Movie = require('../model/Movies')
 
 class MovieController {
+
+    // READ
     async lista(req, res){
         await Movie.find().lean().then(filmes => {
             res.send(filmes)
         }).catch(err => {
             res.send('Lista de filmes vazias')
+        })
+    }
+
+    // CREATE
+    async read(req, res){        
+        let newMovie = {
+            nome: req.body.nome,
+            description: req.body.description
+        }
+    
+        await new Movie(newMovie).save().then(() => {
+            res.send('Filme Cadastrado com sucesso')
+        }).catch(err => {
+            res.send('Erro ao cadastrar novo filme')
+        })
+    }
+
+    // UPDATE
+    async update(req, res) {
+        await Movie.findById(req.body._id).then(filme=> {        
+            filme.nome = req.body.nome
+            filme.save().then(() => {
+                res.send('Filme auterado com sucesso')
+            })
+        }).catch(err => {
+            res.send('Erro ao alterar filme')
+        })
+    }
+
+    // DELETE
+    async delete(req, res){
+        Movie.deleteOne({_id: req.params.id}).then(() => {
+            res.send('Tarefa deletada com sucesso')
+        }).catch(err => {
+            res.send('Erro ao deletar tarefa')
         })
     }
 }
--- backend/backend-catalogo-express-generator/routes/movies.js
+++ backend/backend-catalogo-express-generator/routes/movies.js
@@ -1,42 +1,11 @@
 
+const MovieController = require('../controllers/movies-controllers')
 const express = require('express')
 const router = express.Router()
 
-const Movie = require('../model/Movies')
-const MovieController = require('../controllers/movies-controllers')
-
 router.get('/', MovieController.lista)
-
-router.post('/create', (req, res) => {
-    const newMovie = {
-        nome: req.body.nome,
-        description: req.body.description
-    }
-
-    new Movie(newMovie).save().then(() => {
-        res.send('Filme Cadastrado com sucesso')
-    }).catch(err => {
-        res.send('Erro ao cadastrar novo filme')
-    })
-})
-
-router.put('/update', (req, res) => {
-    Movie.findById(req.body._id).then(filme=> {        
-        filme.description = req.body.description
-        filme.save().then(() => {
-            res.send('Filme auterado com sucesso')
-        })
-    }).catch(err => {
-        res.send('Erro ao alterar filme')
-    })
-})
-
-router.delete('/delete/:id', (req, res) => {
-    Movie.deleteOne({_id: req.params.id}).then(() => {
-        res.send('Tarefa deletada com sucesso')
-    }).catch(err => {
-        res.send('Erro ao deletar tarefa')
-    })
-})
+router.post('/create', MovieController.read)
+router.put('/update', MovieController.update)
+router.delete('/delete/:id', MovieController.delete)
 
 module.exports = router
