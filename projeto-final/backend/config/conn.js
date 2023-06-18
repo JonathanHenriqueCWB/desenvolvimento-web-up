3f43d932a225a68f65c10329d796e49e6e0c2bc0
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2023-06-18 17:49:57 -0300

    migrando banco de dados

--- projeto-final/backend/config/conn.js
+++ projeto-final/backend/config/conn.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 async function main() {
     try {        
-        await mongoose.connect('mongodb://127.0.0.1/ecommerce')
+        await mongoose.connect('mongodb+srv://jonathanhenriquecwb:pTe0c7JVPzp2j8Lc@cluster0.iltswy5.mongodb.net/?retryWrites=true&w=majority')
         console.log('Conectado ao banco de dados com sucesso')
     } catch (error) {
         console.log('Erro ao conectar ao banco de dados' + error)
@@ -10,3 +10,8 @@ async function main() {
 }
 
 module.exports = main
+
+// Atlas
+// jonathanhenriquecwb
+// pTe0c7JVPzp2j8Lc
+// view ip: https://whatismyipaddress.com/
