09bc8da87be684b86f66c739558e4b31606b782e
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2023-06-19 17:47:46 -0300

    ajustes nas models

--- projeto-final/backend/models/Categoria.js
+++ projeto-final/backend/models/Categoria.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose
 
 const categoriaSchema = new Schema({
-    codigo: {type: Number},
-    nome: {type: String},
-    descricao: {type: String}
+    codigo: {type: Number, required: true, unique: true},
+    nome: {type: String, required: true},
+    descricao: {type: String, required: true}
 }, {timestamps: true})
 
 const Categoria = mongoose.model("Categoria", categoriaSchema)
--- projeto-final/backend/models/Cliente.js
+++ projeto-final/backend/models/Cliente.js
@@ -2,22 +2,22 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose
 
 const clienteSchema = new Schema({
-    codigo : {type: String},
-    nome: {type: String},
-    sobrenome: {type: String},
-    cpf: {type: String},    
+    codigo : {type: Number, required: true, unique: true},
+    nome: {type: String, required: true},
+    sobrenome: {type: String, required: true},
+    cpf: {type: String, required: true},    
     foto: {
         data: Buffer,
         contentType: String
     },
 
-    endereco: {type: String},
-    cidade: {type: String},
-    estado: {type: String},
+    endereco: {type: String, required: true},
+    cidade: {type: String, required: true},
+    estado: {type: String, required: true},
     
-    cartao: {type: String},
-    email: {type: String},
-    senha: {type: String},
+    cartao: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    senha: {type: String, required: true},
 
 }, {timestamps: true})
 
