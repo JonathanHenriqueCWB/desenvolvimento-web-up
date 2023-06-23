74c1c969cd6365bd0b945efac1d4c1216966a4f4
Monica de Souza <topalgumacoisatop@gmail.com>
2023-06-23 02:46:13 -0300

    componente de usuarios (cadastro)

---
+++ projeto-final/frontend/src/components/usuarios/Usuarios.jsx
@@ -0,0 +1,9 @@
+const Usuarios = props => {
+    return (
+        <>
+            <h2>Formulario de cadastro</h2>
+        </>
+    )
+}
+
+export default Usuarios
--- projeto-final/frontend/src/pages/usuario.js
+++ projeto-final/frontend/src/pages/usuario.js
@@ -1,7 +1,10 @@
+import UsuariosLogin from '../components/usuarios/Usuarios'
+
 const Usuario = props => {
     return (
         <>
             <h1>Cadastro de usuarios</h1>
+            <UsuariosLogin />
         </>
     )
 }
